1e6c83c3a01c0551e1a30ac43188b9b617fade08
Teshu <152328503+teshu0@users.noreply.github.com>
2026-01-09 13:01:53 +0900

    fix: 誤字

--- app/src/main/java/io/numberrun/Game/Level/Level.java
+++ app/src/main/java/io/numberrun/Game/Level/Level.java
@@ -3,6 +3,6 @@ package io.numberrun.Game.Level;
 import io.numberrun.Component.Component;
 
 // ステージエンティティ
-public class Lavel implements Component {
+public class Level implements Component {
     // オブジェクトの生成に関する情報を持っておくか？
 }
--- app/src/main/java/io/numberrun/Game/Level/LevelSystem.java
+++ app/src/main/java/io/numberrun/Game/Level/LevelSystem.java
@@ -3,7 +3,7 @@ package io.numberrun.Game.Level;
 import io.numberrun.System.GameSystem;
 
 // レーン上に壁とか色々配置するシステム
-public class LavelSystem implements GameSystem {
+public class LevelSystem implements GameSystem {
 
     // TODO: レーンに壁などのオブジェクトを生成する処理と World に spawn する処理
 }
